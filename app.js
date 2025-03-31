3e1efe9b8e6c197fc84eed90725414f983cc0d64
Sudip Kunwar <kunwarsudip339@gmail.com>
2025-03-31 18:29:56 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+function check() {
+  const randomNumber = Math.random();
+
+  if (randomNumber>=0 && randomNumber < 1/3) {
+    return "rock";
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function compare(condition) {
+  const computerMove = check();
+  const checking = condition;
+  let result;
+  
+
+  if(computerMove === checking ){
+    result = 'Tie.'
+  }
+  else if (
+    checking === 'rock' && computerMove === 'scissors' ||
+    checking === 'paper' && computerMove === 'rock' ||
+    checking === 'scissors' && computerMove === 'paper' 
+  )
+  {
+    result = 'You win';
+  }
+
+  else{
+    result = 'Computer wins';
+  }
+
+  alert(`You picked ${checking}. Computer picked ${computerMove}. ${result}`);
+}
