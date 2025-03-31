e154848550343a469d53bf5009f20b48fb72d578
Sudip Kunwar <kunwarsudip339@gmail.com>
2025-03-31 19:16:27 +0545

    styling

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function check() {
   const randomNumber = Math.random();
 
-  if (randomNumber>=0 && randomNumber < 1/3) {
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
     return "rock";
-  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
     return "paper";
   } else {
     return "scissors";
@@ -14,23 +14,19 @@ function compare(condition) {
   const computerMove = check();
   const checking = condition;
   let result;
-  
 
-  if(computerMove === checking ){
-    result = 'Tie.'
-  }
-  else if (
-    checking === 'rock' && computerMove === 'scissors' ||
-    checking === 'paper' && computerMove === 'rock' ||
-    checking === 'scissors' && computerMove === 'paper' 
-  )
-  {
-    result = 'You win';
-  }
-
-  else{
-    result = 'Computer wins';
+  if (computerMove === checking) {
+    result = "Tie.";
+  } else if (
+    (checking === "rock" && computerMove === "scissors") ||
+    (checking === "paper" && computerMove === "rock") ||
+    (checking === "scissors" && computerMove === "paper")
+  ) {
+    result = "You win";
+  } else {
+    result = "Computer wins";
   }
 
+  output(computerMove, checking);
   alert(`You picked ${checking}. Computer picked ${computerMove}. ${result}`);
 }
